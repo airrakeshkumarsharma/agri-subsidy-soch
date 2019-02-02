e062d7d67032418811d3bbbe13f34ef1d1b79531
Rakesh Kumar Sharma <airrakeshkumarsharma>
2019-02-02 14:01:34 +0530

    db is added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var monk = require('monk');  //get the monk javascript
+var moment = require('moment'); //get the moment data
+var dbs=monk('agri:Ramayana123@ds119765.mlab.com:19765/agri');
+mongodb://<dbuser>:<dbpassword>@ds119765.mlab.com:19765/agri
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
