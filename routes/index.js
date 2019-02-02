af5143a01083b4757cfa5eb39a0ab77c6c68bfac
Rakesh Kumar Sharma <airrakeshkumarsharma>
2019-02-02 13:45:18 +0530

    upload is added

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/upload', function(req, res){
+	res.render("upload");
+});
 module.exports = router;
